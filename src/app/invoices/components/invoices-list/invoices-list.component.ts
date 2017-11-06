12e26f222a9fc9bf5f3a39c44d752001b3e41b86
vipul <vipul>
2017-11-06 13:56:09 +0530

    invoices list

--- src/app/invoices/components/invoices-list/invoices-list.component.ts
+++ src/app/invoices/components/invoices-list/invoices-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-invoices-list',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoices-list.component.scss']
 })
 export class InvoicesListComponent implements OnInit {
-
+  @ViewChild('myTable') table: any;
+   rows = [];
+   temp = [];
+   isLimits: number = 10;
+   records: any;
+   
   constructor() { }
 
   ngOnInit() {
+    this.rows = [
+      ];
   }
 
 }
+
+
+
