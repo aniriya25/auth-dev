8ea0ea08811177902b6d2035718374c98db58146
vipul <vipul>
2017-11-20 11:43:27 +0530

    invoices

--- src/app/transactions/components/summary/summary.component.ts
+++ src/app/transactions/components/summary/summary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-summary',
@@ -6,17 +7,55 @@ import { Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
-  @ViewChild('myTable') table: any;
-  rows = [];
-  temp = [];
-  isLimits: number = 10;
-  records: any;
-  constructor() { }
-
-  ngOnInit() {
-    this.rows = [
-      { "company": "", "service": "", "comment": "" },
-      ];
-  }
+  
+  
+  rows = [
+    {}
+  ];
+  
+    temp = [];
+  
+    columns = [
+      { prop: 'name' },
+      { name: 'Company' },
+      { name: 'Gender' }
+    ];
+    @ViewChild(SummaryComponent) table: SummaryComponent;
+    constructor() {
+      this.fetch((data) => {
+        // cache our list
+        this.temp = [...data];
+  
+        // push our inital complete list
+        this.rows = data;
+      });
+    }
+  
+    fetch(cb) {
+      const req = new XMLHttpRequest();
+      req.open('GET', `assets/data/company.json`);
+  
+      req.onload = () => {
+        cb(JSON.parse(req.response));
+      };
+  
+      req.send();
+    }
+  
+    updateFilter(event) {
+      debugger
+      const val = event.target.value.toLowerCase();
+  
+      // filter our data
+      const temp = this.temp.filter(function(d) {
+        return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+      });
+  
+      // update the rows
+      this.rows = temp;
+      // Whenever the filter changes, always go back to the first page
+      // this.table.offset = 0;
+    }
+    ngOnInit() {}
 
 }
